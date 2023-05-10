fb06bcd730f4616ac7f9c2ae33ea217aef984b84
SERGIO TEJEDOR MORENO <119494713+sertemo@users.noreply.github.com>
2023-05-10 16:56:43 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,79 @@
+import streamlit as st
+from llm_logics import convert_mail
+
+st.set_page_config(
+    page_title="TransMailApp",
+    page_icon="💻",
+    #layout="wide",
+    #initial_sidebar_state="collapse",
+)
+
+st.title("TransMailApp")
+st.markdown("Aplicación web para formalizar y traducir emails.")
+
+#"st.session_state_object:", st.session_state
+
+user_email = st.experimental_user["email"]
+user_acount = st.experimental_user["email"].split("@")[0]
+st.info(f"Bienvenido, {user_acount} !")
+
+if st.session_state.get("email_input",None):
+    st.caption("último mail enviado:")
+    st.caption(st.session_state.email_input[:50]+"...")
+
+st.header("Introduce un texto para formalizar y traducir")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    language = st.selectbox(
+        "Escoge el idioma al que traducir",
+        ("Inglés","Francés"),
+        index=1,
+    )
+
+lang_map ={
+    "Inglés" : "english",
+    "Francés" : "french",    
+}
+
+language = lang_map[language]
+
+with col2:
+    opcion_llm = st.selectbox(
+        "Escoge el modelo a emplear",
+        ("openai",)
+    )
+
+temperature = st.slider("Escoger un valor de aleatoriedad",min_value=0.0,max_value=1.0,step=0.1,key="temp")
+
+def contar_palabras():
+    num_words = len(st.session_state["email_input"].split())
+    st.session_state["num_words"] = num_words
+    return num_words
+
+def get_text():
+    email = st.text_area(
+        label="Email Input",
+        label_visibility='collapsed',
+        placeholder="Escribe aquí tu email...",
+        key="email_input",
+        on_change=contar_palabras)
+    
+    return email
+
+email = get_text()
+
+nwords = st.session_state.get("num_words",0)
+st.caption(f"Palabras: {nwords}/700")
+
+if len(email.split()) > 700:
+    st.error("El email es demasiado largo.",icon="🛑")
+    st.stop()
+
+if email:
+    with st.spinner("Pensando..."):
+        response = convert_mail(language=language,email=email,llm_source=opcion_llm,temperatura=temperature)
+    st.divider()
+    st.subheader("Email respuesta:")
+    st.write(response)
---
+++ llm_logics.py
@@ -0,0 +1,73 @@
+from dotenv import dotenv_values
+from langchain.llms import HuggingFaceHub
+from langchain import PromptTemplate
+from langchain import OpenAI
+from typing import Literal
+
+
+#Cargamos variables de entorno
+config = dotenv_values(".env")
+HUGGINGFACE_API_KEY = config["HUGGINGFACE_API_KEY"]
+OPENAI_API_KEY = config["OPENAI_API_KEY"]
+
+template = """
+    Below is an email that may be poorly worded.
+    This email is written in spanish.
+    Your goal is to:
+    - Translate the input text to a specified language.
+    - Convert the input text to a very formal tone.
+    - Deliver the response in an email format.
+    
+    Below is the email,  and language:
+    LANGUAGE: {language}
+    EMAIL: {email}
+    
+    YOUR RESPONSE:
+"""
+""" #Definimos el modelo
+llm_hug = HuggingFaceHub(
+    #repo_id="google/flan-t5-xxl",
+    model_kwargs={
+        "temperature":0.5,
+        "max_length":512},
+        huggingfacehub_api_token=HUGGINGFACE_API_KEY
+) """
+
+prompt = PromptTemplate(
+    input_variables=["language","email"],
+    template=template
+)
+
+def convert_mail(
+        language:Literal["french","english"],
+        email:str,
+        temperatura:float,
+        llm_source:Literal["openai","huggingface"]="openai",
+        )->str:
+    
+    """ Dados un email, un llm y un idioma, devuelve el mismo email,\n
+    en lenguaje formal y traducido al idioma seleccionado
+     
+    Parameters
+    ----------
+    *language* : idioma para traducir el email
+
+    *email* : El email a formatear y traducir
+
+    *llm* : el modelo que se usará para la traducción: 'openai' o 'huggingface'
+    Actualmente el modelo de huggingface no funciona.
+     """
+    #Definimos modelo OpenAI
+    llm_open = OpenAI(
+    model_name="text-davinci-003",
+    temperature=temperatura,
+    openai_api_key=OPENAI_API_KEY
+    )
+
+    llm = llm_open if llm_source == "openai" else None
+    format_prompt = prompt.format(language=language,email=email)
+    #print(f"Prompt que se enviará a la API del modelo {llm.__class__.__name__}:\n{format_prompt}")
+    return llm(format_prompt)
+
+if __name__ == '__main__':
+    pass
